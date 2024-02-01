03cf0ada8095cde27ff000546faae3229dbee4d6
Anna Ignateva <gannaioannovna25@gmail.com>
2024-02-01 13:37:42 +0400

    second commit

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Movies } from '../components/Movies';
+import { Preloader } from '../components/Preloader';
+import { Search } from '../components/Search';
+
 class Main extends React.Component {
   state = {
     movies: [],
@@ -11,16 +14,19 @@ class Main extends React.Component {
       .then((data) => this.setState({ movies: data.Search }));
   }
 
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?apikey=78584b3c&s=${str}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  };
+
   render() {
     const { movies } = this.state;
 
     return (
       <main className="container content">
-        {movies.length ? (
-          <Movies movies={this.state.movies} />
-        ) : (
-          <h3>Loading</h3>
-        )}
+        <Search searchMovies={this.searchMovies} />
+        {movies.length ? <Movies movies={this.state.movies} /> : <Preloader />}
       </main>
     );
   }
